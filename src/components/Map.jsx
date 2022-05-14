42dad2fbe4fd6d3d958d7d471d793b0d1eb177db
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-05-14 14:42:15 -0700

    add: Map Component

---
+++ src/components/Map.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div>This component will be the map</div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
+import Map from "../components/Map";
 import Navbar from "../components/navbar";
 
 const Home = () => {
   return (
     <div className="content">
       <Navbar />
-      <h2>Home</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quisquam
-        ducimus eligendi iste sunt? Iste provident quisquam voluptatem, possimus
-        excepturi minima quis nemo culpa aut impedit eveniet. Nemo, eum neque!
-      </p>
+      <Map/>
     </div>
   );
 };
