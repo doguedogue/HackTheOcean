34e16624738cc11679d6c3f254fa50c0f496da9a
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-05-14 19:27:40 -0700

    delete: corrupt components

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -12,14 +12,8 @@ export default function Map() {
     <div>
       <MapContainer center={position} zoom={5}>
       <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       </MapContainer>
     </div>
   )
