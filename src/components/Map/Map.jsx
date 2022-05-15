b0abb68d329e5100ea6982a224069c372eec90f9
Christopher Perez <chistoperez@gmail.com>
2022-05-15 11:35:45 -0500

    getLocation by click

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import './Map.css'
+import "./Map.css";
+import { useMapEvents } from "react-leaflet/hooks";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-
-
+const LocationFinder = () => {
+  const map = useMapEvents({
+    click(e) {
+      console.log(e.latlng);
+      const LatLng = e.latlng;
+      return LatLng;
+    },
+  });
+};
 
 export default function Map() {
-  const position = [20.3229714, -103.8999308]
+  const position = [20.3229714, -103.8999308];
+
   return (
     <div>
       <MapContainer center={position} zoom={5}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <LocationFinder />
       </MapContainer>
     </div>
-  )
+  );
 }
