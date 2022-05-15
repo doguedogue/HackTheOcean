5ce41ab1e001e95e6b6148034e989667ca7caef0
Alfredo Dominguez <thecatura.ad@gmail.com>
2022-05-14 19:13:41 -0700

    add: simple map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './Map.css'
+
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+
 
 export default function Map() {
+  const position = [20.3229714, -103.8999308]
   return (
-    <div>This component will be the map</div>
+    <div>
+      <MapContainer center={position} zoom={5}>
+      <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+      </MapContainer>
+    </div>
   )
 }
